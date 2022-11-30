015b27de28f3ac70cbb4e026bfb4bae2d75fd3ff
RonFed <ronfederman@mail.tau.ac.il>
2022-11-30 14:30:43 +0200

    Implementing unstruction queue

--- sim/Architecure_Scoreboard/main.c
+++ sim/Architecure_Scoreboard/main.c
@@ -5,15 +5,16 @@
 #include "memin.h"
 #include "cfg.h"
 #include "init.h"
+#include "inst_queue.h"
 
 uint32_t        q_mem_arr[MAX_ASSEMBLY_LINES]; /* memory array contains both imem and dmem */
 config_t        g_config;                   /* Configuration paramaters */
 unit_t**        g_op_units;
 reg_val_status  g_regs[REGS_NUM];
+inst_queue_t    g_inst_queue;
 
 int main(int argc, char* argv[]) {
 
-    printf(argv[0]);
     FILE* mem_file = open_and_validate_file(argv[2], "r");
     load_memin(mem_file, q_mem_arr);
 
@@ -23,6 +24,8 @@ int main(int argc, char* argv[]) {
     g_op_units = init_units(&g_config);
 
     init_regs_status_values(g_regs);
+
+    init_instruction_queue(&g_inst_queue);
 
     while(1);
 }
