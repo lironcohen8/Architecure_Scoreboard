84f222de52e5c685249202eba6a5c1a5e84c035d
Liron <lironcohen3@mail.tau.ac.il>
2022-12-01 10:06:34 +0200

    vcxproj and minor changes

--- sim/Architecure_Scoreboard/memin.c
+++ sim/Architecure_Scoreboard/memin.c
@@ -1,23 +1,6 @@
 #include "global.h"
 #include "memin.h"
 
-// THIS IS THE DECODE FUNCTION - SHOULD BE CALLED WITH ISSUE STAGE
-static void parse_line_to_cmd(char* line, inst_t* cmd) {
-    /* Each command is 32 bits*/
-    uint32_t raw;
-    sscanf_s(line, "%lX", &raw);
-
-    /* construct the command object */
-    cmd->imm = raw & 0xFFF;
-    cmd->src1 = (raw >> 12) & 0xF;
-    cmd->src0 = (raw >> 16) & 0xF;
-    cmd->dst = (raw >> 20) & 0xF;
-    cmd->opcode = (raw >> 24) & 0xF;
-
-    cmd->raw_inst = raw;
-}
-
-
 void load_memin(FILE* instr_file, uint32_t* imem_buff) {
     char line_buffer[INSTRUCTION_LINE_LEN + 2];
     int memory_count = 0;
@@ -28,4 +11,16 @@ void load_memin(FILE* instr_file, uint32_t* imem_buff) {
         sscanf_s(line_buffer, "%lX", &curr_mem);
         imem_buff[memory_count++] = curr_mem;
     }
+}
+
+// THIS IS THE DECODE FUNCTION - SHOULD BE CALLED WITH ISSUE STAGE
+static void parse_line_to_inst(uint32_t raw, inst_t* inst) {
+    /* construct the command object */
+    inst->imm = raw & 0xFFF;
+    inst->src1 = (raw >> 12) & 0xF;
+    inst->src0 = (raw >> 16) & 0xF;
+    inst->dst = (raw >> 20) & 0xF;
+    inst->opcode = (raw >> 24) & 0xF;
+
+    inst->raw_inst = raw;
 }
