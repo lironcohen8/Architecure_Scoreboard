534c470c427d12b73beb6b7f4606543410ac181d
Liron <lironcohen3@mail.tau.ac.il>
2022-12-08 08:43:36 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/lironcohen8/Architecure_Scoreboard""
    This reverts commit afb8481a64331968a12b34ba3d5cc6e44c7cc302.

--- sim/Architecure_Scoreboard/main.c
+++ sim/Architecure_Scoreboard/main.c
@@ -5,14 +5,19 @@
 #include "init.h"
 #include "scoreboard.h"
 
-simulation_t    g_simulation;
-
 int main(int argc, char* argv[]) {
     FILE* mem_file = open_and_validate_file(argv[2], "r");
     
     FILE* cfg_file = open_and_validate_file(argv[1], "r");
     
-    init_simulation(&g_simulation, mem_file, cfg_file);
+    init_simulation(get_simulation(), mem_file, cfg_file);
 
-    while(1);
+    while (!is_halted() || (num_of_active_instructions() > 0)) {
+        if (fetch()) {
+            // Advance PC after succesfull fetch
+            advance_pc();
+        }
+
+        // TODO...
+    }
 }
--- sim/Architecure_Scoreboard/output_files.h
+++ sim/Architecure_Scoreboard/output_files.h
@@ -9,6 +9,6 @@ void write_memout_file(FILE* memout_file, uint32_t* g_mem_arr);
 
 void write_regout_file(FILE* regout_file, reg_val_status* g_regs);
 
-void write_traceinst_file(FILE* traceinst_file, inst_t* inst);
+void write_traceinst_file(FILE* traceinst_file, inst_t* inst, uint32_t issued_count);
 
 void write_traceunit_file(FILE* traceunit_file, int cycle, unit_t* trace_unit);
